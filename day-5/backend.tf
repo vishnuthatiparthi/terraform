16324b9ea09dd2aecee8e58702869a104df63083
simonxox <simonchinnu13@gmail.com>
2025-07-10 11:42:40 +0530

    Revert "lol"
    This reverts commit 319c3da6af0d7d409a14ff7765be7c6d2caaec87.

--- day-5/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "simonjack"
-    key    = "terraform.state"
-    region = "ap-south-1"
-  }
-}
--- day-5/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_instance" "name" {
-  ami = "ami-0d03cb826412c6b0f"
-  instance_type = "t2.micro"
-}
