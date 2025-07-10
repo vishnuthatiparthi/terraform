dda14365041977da0d1c67ac6e09a5852e8e00f5
simonxox <simonchinnu13@gmail.com>
2025-07-10 12:00:34 +0530

    kok

--- day-5/main.tf
+++ day-5/main.tf
@@ -1,5 +1,16 @@
 resource "aws_instance" "name" {
-  ami = "ami-0d03cb826412c6b0f"
+  ami           = "ami-0d03cb826412c6b0f"
   instance_type = "t2.micro"
 
+  tags = {
+    Name = "hellofromterra"
+  }
+}
+
+resource "aws_vpc" "name2" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "simon"
+  }
 }
