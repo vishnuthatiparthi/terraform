3e3edd50326a58480f2f666f08e765abe742d6e4
vishnuthatiparthi <vishnureddys12345@gmail.com>
2025-07-11 11:13:46 +0530

    day-5

---
+++ day-5/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "name" {
+    ami = "ami-05ffe3c48a9991133"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "Dev-1"
+    }
+  
+}
+resource "aws_s3_bucket" "name" {
+    bucket = "tombayya-1"
+  
+}
