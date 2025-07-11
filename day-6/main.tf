423a99ead8ab6422d88ecfe51e574eed52c94997
vishnuthatiparthi <vishnureddys12345@gmail.com>
2025-07-11 14:15:46 +0530

    kkk

--- day-6/main.tf
+++ day-6/main.tf
@@ -1,10 +1,16 @@
 resource "aws_instance" "name" {
     ami = "ami-05ffe3c48a9991133"
     instance_type = "t2.micro"
+    tags = {
+      Name = "viva"
+    }
   
 }
 resource "aws_instance" "name1" {
     ami = "ami-05ffe3c48a9991133"
     instance_type = "t2.micro"
+    tags = {
+      Name = "simon"
+    }
   
 }
